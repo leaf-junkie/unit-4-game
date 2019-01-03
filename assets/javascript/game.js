c9fe3780201e35557e6e4ff9a6b9aa45cf291233
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-03 18:14:17 -0500

    Psuedocoding
    Added pseudocode for onclick event
    Defined gems as variables and assiegned values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,39 @@
 // Goal number: 39
 // Crystal values: 1, 4, 5, 6
+
+let gemA = 1;
+let gemB = 4;
+let gemC = 5;
+let gemD = 6;
+
+// This is the game logic
+
+// When the user clicks a button, the value of the
+// appropriate gem is added to the Current Score
+$('btn').on('click') {
+
+}
+
+// Check to see if the Current Score is equal to 
+// the Goal
+// if (currentScore < Goal) {
+    // return;
+// } else if () currentScore === Goal {
+    //  win();
+// } else {
+    // lose()
+// }
+
+// These are the functions
+
+// function win() {
+
+// }
+
+// function lose() {
+
+// }
+
+// function reset() {
+
+// }
