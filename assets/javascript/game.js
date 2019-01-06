45bc955dafeb24fae4c07d8805ad622f47a1d26c
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-06 02:03:09 -0500

    Smoothing out the kinks in JavaScript
    The user's goal number is correctly displaying in the DOM
    The goal number and values of each gem are being logged to the console
    Need to work on click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
 // Generate a random integer (Goal number) between 29 and __ for each game
-var randomNum = Math.floor(Math.random()*101+29);
-
-// Console log random
-console.log(randomNum);
+var goal = Math.floor(Math.random()*100 + 21);
+console.log(goal);
+$('#goal').text(goal);
 
 // Generate random value between 1 and 12 for each gem
-let gemA = Math.floor(Math.random()*11+1);
-let gemB = Math.floor(Math.random()*11+1);
-let gemC = Math.floor(Math.random()*11+1);
-let gemD = Math.floor(Math.random()*11+1);
+let gemA = Math.floor(Math.random()*10 + 1);
+let gemB = Math.floor(Math.random()*10 + 1);
+let gemC = Math.floor(Math.random()*10 + 1);
+let gemD = Math.floor(Math.random()*10 + 1);
+console.log('Gem A: ' + gemA + ' | Gem B: ' + gemB + ' | Gem C: ' + gemC + ' | Gem D: ' + gemD);
 
 // Variable declarations for score values
 let wins = 0;
@@ -22,36 +22,48 @@ $('#lossCount').text(losses);
 
 // This is the game logic
 
-// When the user clicks a button, the value of the
-// appropriate gem is added to the Current Score
-$('#gemA').on('click', ()=> {
-    currentScore = currentScore + gemA;
-    console.log("Current Score: " + currentScore);
-});
+// $('#gemA').on('click', ()=> {
+//     currentScore = currentScore + gemA;
+//     console.log('Current Score: ' + currentScore);
+//     $('#currentScore').text();
+// });
 
-$('#gemB').on('click', ()=> {
-    currentScore = currentScore + gemB;
-    console.log("Current Score: " + currentScore);
-});
+// $('#gemB').on('click', ()=> {
+//     currentScore = currentScore + gemB;
+//     console.log('Current Score: ' + currentScore);
+// });
 
-$('#gemC').on('click', ()=> {
-    currentScore = currentScore + gemC;
-    console.log("Current Score: " + currentScore);
-});
+// $('#gemC').on('click', ()=> {
+//     currentScore = currentScore + gemC;
+//     console.log('Current Score: ' + currentScore);
+// });
 
-$('#gemD').on('click', ()=> {
-    currentScore = currentScore + gemD;
-    console.log("Current Score: " + currentScore);
-});
+// $('#gemD').on('click', ()=> {
+//     currentScore = currentScore + gemD;
+//     console.log('Current Score: ' + currentScore);
+// });
 
 // Check to see if the Current Score is equal to the goal number
-// if (currentScore < Goal) {
-    // return;
-// } else if () currentScore === Goal {
-    //  win();
-// } else {
-    // lose()
-// }
+if ($('#currentScore') < goal) {
+
+    // When user clicks button, add value to current score
+    for (var i = 0; i < 4; i++) {
+        $('.gems').on('click', ()=> {
+        total += currentScore; //parseInt
+        console.log('Your current score is: ' + currentScore);
+        $('#currentScore').text();
+        });
+    }
+    return;
+    
+} else if ($('#currentScore') > goal) {
+    lose();
+    console.log('You lose!');
+
+} else if (currentScore === goal) {
+     win();
+     console.log('You win!');
+}
 
 // These are the functions
 
@@ -60,11 +72,10 @@ $('#gemD').on('click', ()=> {
 
         // Increment wins by 1
         wins ++;
-        console.log('Number of wins: ' + wins);
+        console.log('You have won ' + wins + ' many times');
 
         // Update number of wins in the DOM
         $('#winCount').text(wins);
-
         reset();
     }
 
@@ -73,11 +84,10 @@ $('#gemD').on('click', ()=> {
 
         // Increment losses by 1
         losses ++;
-        console.log('Number of losses: ' + losses);
+        console.log('You have lost ' + losses + ' many times');
 
         // Update number of losses in the DOM
         $('#lossCount').text(losses);
-
         reset();
     }
 
